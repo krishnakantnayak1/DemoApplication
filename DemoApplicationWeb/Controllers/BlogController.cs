a2558fe0a76e34afd4db2a72cf2e4e01f853173d
Krishnakant Nayak <krishnakant@enjoywishlist.com>
2024-05-03 12:10:08 +0530

    CRUD apis

--- DemoApplicationWeb/Controllers/BlogController.cs
+++ DemoApplicationWeb/Controllers/BlogController.cs
@@ -1,11 +1,11 @@
 ﻿using DemoApplicationWeb.Data;
 using DemoApplicationWeb.Models;
 using Microsoft.AspNetCore.Mvc;
-
 namespace DemoApplicationWeb.Controllers
 {
     public class BlogController : Controller
     {
+
         private readonly ApplicationDBContext _db;
 
         public BlogController(ApplicationDBContext db) {  _db = db; }
@@ -26,6 +26,29 @@ namespace DemoApplicationWeb.Controllers
             }
             return View(obj);
             
+        }
+        public Blog Get(int id)
+        {
+            return _db.Blogs.Find(id);
+        }
+        [HttpPost]
+        public Blog Edit(Blog blog)
+        {
+            Blog x = _db.Blogs.Find(blog.id);
+            if(x != null) {
+                x.Content = blog.Content;
+                _db.SaveChanges();
+            }
+            return x;
+        }
+
+        public void Delete(int id)
+        {
+            Blog b=_db.Blogs.Find(id);
+            if(b != null) { 
+            _db.Blogs.Remove(b);
+            _db.SaveChanges();
+            }
         }
     }
 }
